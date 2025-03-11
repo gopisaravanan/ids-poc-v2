670f23cecb9c6427ae637a1baa1e6fcc3cedc294
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-11 14:52:33 +0530

    tailwind config added in vite config for button package

--- packages/Button/vite.config.ts
+++ packages/Button/vite.config.ts
@@ -12,12 +12,14 @@
   import react from '@vitejs/plugin-react-swc'
   import dts from 'vite-plugin-dts'
   import { libInjectCss } from 'vite-plugin-lib-inject-css'
+  import tailwindcss from '@tailwindcss/vite'
 
   const __dirname = dirname(fileURLToPath(import.meta.url))
 
   export default defineConfig({
     plugins: [
       react(),
+      tailwindcss(),
       libInjectCss(),
       dts({
         tsconfigPath: resolve(__dirname, "tsconfig.json"),
