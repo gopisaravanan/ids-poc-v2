5f33eeb4cb64e7d1efde3ad5edaef05c9c6fc40d
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-11 15:23:19 +0530

    cva, tailwindcss test

--- packages/Button/lib/Button.tsx
+++ packages/Button/lib/Button.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
+// import { cva, type VariantProps } from "class-variance-authority";
 
-export interface ButtonProps extends VariantProps<typeof buttonVariants> {
+// export interface ButtonProps extends VariantProps<typeof buttonVariants> {
+export interface ButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
@@ -9,31 +10,35 @@ export interface ButtonProps extends VariantProps<typeof buttonVariants> {
   size?: "sm" | "md" | "lg" | null | undefined
 }
 
-const buttonVariants = cva(
-  "px-4 py-2 text-white border-none rounded-md cursor-pointer transition-all",
-  {
-    variants: {
-      variant: {
-        primary: "bg-violet-600 hover:bg-violet-700",
-        secondary: "bg-gray-600 hover:bg-gray-700",
-        danger: "bg-red-600 hover:bg-red-700",
-      },
-      size: {
-        sm: "px-2 py-1 text-sm",
-        md: "px-4 py-2 text-base",
-        lg: "px-6 py-3 text-lg",
-      },
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-    },
-  }
-);
+// const buttonVariants = cva(
+//   "px-4 py-2 text-white border-none rounded-md cursor-pointer transition-all",
+//   {
+//     variants: {
+//       variant: {
+//         primary: "bg-violet-600 hover:bg-violet-700",
+//         secondary: "bg-gray-600 hover:bg-gray-700",
+//         danger: "bg-red-600 hover:bg-red-700",
+//       },
+//       size: {
+//         sm: "px-2 py-1 text-sm",
+//         md: "px-4 py-2 text-base",
+//         lg: "px-6 py-3 text-lg",
+//       },
+//     },
+//     defaultVariants: {
+//       variant: "primary",
+//       size: "md",
+//     },
+//   }
+// );
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, variant, size, className }) => {
+// const Button: React.FC<ButtonProps> = ({ children, onClick, variant, size, className }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
-    <button className={buttonVariants({ variant, size, className })} onClick={onClick}>
+    <button
+        className="px-4 py-2 bg-violet-600 text-white border-none rounded-md cursor-pointer"
+    // className={buttonVariants({ variant, size, className })}
+    onClick={onClick}>
       {children}
     </button>
   );
