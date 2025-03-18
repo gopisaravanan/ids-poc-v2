96ae3acdf7131aa0dc1083040c0b5859be328f9f
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-18 18:55:43 +0530

    test github package

---
+++ packages/text-v3/lib/TextComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import '../styles/tailwind.css'
+
+const TextComponent: React.FC = () => {
+  return <h1 className="bg-pink-500">Version 2</h1>;
+};
+
+export default TextComponent;
